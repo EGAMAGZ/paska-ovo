3405a05a9c1456cfbbe859dc83437ff5c3d4a2c0
Gamaliel Garcia <46827955+EGAMAGZ@users.noreply.github.com>
2024-04-17 23:02:49 -0600

    refactor!: Changed the value of SPECIAL_KEYS to match to the key value from KeyEvent

--- src/util/constants.ts
+++ src/util/constants.ts
@@ -1,14 +1,14 @@
-export const SPECIAL_KEYS: Record<string, number> = {
-    "slash": 191,
-    "up": 38,
-    "down": 40,
-    "left": 37,
-    "right": 39,
-    "enter": 13,
-    "space": 32,
-    "ctrl": 17,
-    "alt": 18,
-    "tab": 9,
-    "esc": 27
+export const SPECIAL_KEYS: Record<string, string> = {
+    "slash": "/",
+    "up": "ArrowUp",
+    "down": "ArrowDown",
+    "left": "ArrowLeft",
+    "right": "ArrowRight",
+    "enter": "Enter",
+    "space": "Space",
+    "ctrl": "Control",
+    "alt": "Alt",
+    "tab": "Tab",
+    "esc": "Escape",
 } as const;
 
