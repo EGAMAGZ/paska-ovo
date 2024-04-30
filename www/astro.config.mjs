14a0e0f73df71fd6c02d56bc1c9b9e5a509ea490
Gamaliel Garcia <46827955+EGAMAGZ@users.noreply.github.com>
2024-04-30 11:09:26 -0600

    chore: Installed preact, preact signals and paska ov

--- www/astro.config.mjs
+++ www/astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+import preact from "@astrojs/preact";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind(), preact(), mdx()]
 });
