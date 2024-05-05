9d60a087f0d6ed3e932d4ec87c06b4aeef0b53c0
Gamaliel Garcia <46827955+EGAMAGZ@users.noreply.github.com>
2024-05-05 17:31:08 -0600

    ci(deploy): Added workflow to deploy example on github pages

--- example/astro.config.mjs
+++ example/astro.config.mjs
@@ -4,5 +4,7 @@ import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), preact()]
+  integrations: [tailwind(), preact()],
+  site: "https://egamagz.github.io",
+  base: "paska-ovo"
 });
