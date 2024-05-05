dc9bca763d20f3aa9658b5d3252ffefddce5c670
Gamaliel Garcia <46827955+EGAMAGZ@users.noreply.github.com>
2024-05-05 16:57:33 -0600

    refactor: Remove unnecesary content slot

--- example/astro.config.mjs
+++ example/astro.config.mjs
@@ -2,9 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import preact from "@astrojs/preact";
 
-import mdx from "@astrojs/mdx";
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), preact(), mdx()]
+  integrations: [tailwind(), preact()]
 });
